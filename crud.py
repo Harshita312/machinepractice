4fd822c51c789d7d484600ed72cedd31b70995b4
Harshita123 <harshitapolara3@gmail.com>
2024-09-06 21:42:04 +0530

    change

--- crud.py
+++ crud.py
@@ -11,8 +11,55 @@ def insert_data():
     con.commit()
     print("Data inserted...")
 
-# def update_data():
-#     id=int(input("Enter employee id:"))
-#     salary=int(input("Enter employee salary:"))
-#     query="update emp set "
-insert_data()
+def update_data():
+    id=int(input("Enter employee id:"))
+    salary=int(input("Enter employee salary:"))
+    query="update emp set salary={} where id={}".format(salary,id)
+    cursor.execute(query)
+    con.commit()
+    if cursor.rowcount > 0:
+        print("Data updated....")
+    else:
+        print("No data found")
+
+def delete_data():
+    id=int(input("Enter employee id:"))
+    query="delete from emp where id={}".format(id)
+    cursor.execute(query)
+    con.commit()
+    if cursor.rowcount > 0:
+        print("Data deleted....")
+    else:
+        print("No data found")
+
+def read_data():
+    cursor.execute("select * from emp")
+    record=cursor.fetchall()
+    for i in record:
+        print(i)
+    print("Total records:",cursor.rowcount)
+
+while True:
+    print("\nOptions:")
+    print("1. insert data")
+    print("2. update data")
+    print("3. delete data")
+    print("4. read data")
+    print("5. exit")
+
+    ch = input("Enter your choice:")
+
+    if ch=='1':
+        insert_data()
+    elif ch=='2':
+        update_data()
+    elif ch=='3':
+        delete_data()
+    elif ch=='4':
+        read_data()
+    elif ch=='5':
+        break
+    else:
+        print("Invalid choice")
+
+        
